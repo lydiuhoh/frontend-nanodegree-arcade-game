3d5766ee7b57abecf89659f86800567e9ce16d59
Lydia Gerlach <gerlach.lydia@gmail.com>
2019-09-08 17:13:24 -0500

    submission

--- js/app.js
+++ js/app.js
@@ -1,11 +1,9 @@
 // Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
+var Enemy = function(y) {
+    this.x=0;
     this.sprite = 'images/enemy-bug.png';
+    this.y = y;
+    this.speed = Math.floor(Math.random() * 3 + 2);
 };
 
 // Update the enemy's position, required method for game
@@ -14,23 +12,102 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    this.x += this.speed;
+    if(this.x > 500) {
+        this.x = -100;
+ }
 };
 
-// Draw the enemy on the screen, required method for game
+// check if player and enemy collide at the "right" time (not to early or late)
+function checkCollisions() {
+    for (var i = 0; i < allEnemies.length; i++) {
+
+        var xBuffer = 45;
+        var yBuffer = 20;
+
+        if (allEnemies[i].x - xBuffer <= player.x && allEnemies[i].x + xBuffer >= player.x) {
+            if (allEnemies[i].y - yBuffer <= player.y && allEnemies[i].y + yBuffer >= player.y ) {
+                player.reset();
+            }
+        }
+        
+    }
+}
+
+// Draw the enemy on the screen
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
+// Player
+var Player = function() {
+
+    // character
+     this.sprite = 'images/char-princess-girl.png';
+     // placement
+     this.x = 200;
+     this.y = 400;
+     this.width = 30;
+     // movement
+     this.dx = 100;
+     this.dy = 80;
+
+};
+
+// Draw the player on the screen
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+}
+
+Player.prototype.update = function() {
+    // Sets player boundries and resets when you win the game
+    if (this.x > 400) {
+        this.x = 400;
+    } else if (this.x < 0) {
+        this.x = 0;
+    } else if (this.y > 400) {
+        this.y = 400;
+    } else if (this.y < 0) {
+        this.y = 0
+        alert("You made it!")
+        this.reset();
+    }
+};
+
+Player.prototype.reset = function() {
+    this.x = 200;
+    this.y = 400;
+}
+
+// Player movement keys
+Player.prototype.handleInput = function(keys) {
+    if ('up' === keys) {
+        this.y -= this.dy;
+    }
+    if ('down' === keys) {
+        this.y += this.dy;
+    }
+    if ('left' === keys) {
+        this.x -= this.dx;
+    }
+    if ('right' === keys) {
+        this.x += this.dx;
+    }
+};
 
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
+var enemy1 = new Enemy(60);
+var enemy2 = new Enemy(140);
+var enemy3 = new Enemy(220);
 
+var allEnemies = [enemy1, enemy2, enemy3];
+
+
+var player = new Player();
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
--- js/engine.js
+++ js/engine.js
@@ -78,8 +78,8 @@ var Engine = (function(global) {
      * on the entities themselves within your app.js file).
      */
     function update(dt) {
-        updateEntities(dt);
-        // checkCollisions();
+         updateEntities(dt);
+         checkCollisions();
     }
 
     /* This is called by the update function and loops through all of the
@@ -149,7 +149,7 @@ var Engine = (function(global) {
         /* Loop through all of the objects within the allEnemies array and call
          * the render function you have defined.
          */
-        allEnemies.forEach(function(enemy) {
+         allEnemies.forEach(function(enemy) {
             enemy.render();
         });
 
@@ -173,7 +173,7 @@ var Engine = (function(global) {
         'images/water-block.png',
         'images/grass-block.png',
         'images/enemy-bug.png',
-        'images/char-boy.png'
+        'images/char-princess-girl.png'
     ]);
     Resources.onReady(init);
 
